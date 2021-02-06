aade48e70c1ce38d356f2462229370cca1b7bc94
иван кузьмич <ivankuzmic210@gmail.com>
2021-02-07 01:24:45 +0300

    Do multithreading training

---
+++ CsharpEducation/classes/Advanced/Multithreading/MultithreadingBasicTraining.cs
@@ -0,0 +1,24 @@
+﻿using Education.interfaces;
+using System;
+using System.Threading;
+
+namespace Education.classes.Advanced.Multithreading
+{
+    class MultithreadingBasicTraining : ITask
+    {
+        public static void OutTask()
+        {
+            Console.WriteLine("---- class Thread ----");
+            Thread thread = Thread.CurrentThread;
+
+            Console.WriteLine($"Имя потока: {thread.Name}");
+            Console.WriteLine($"Запущен ли поток: {thread.IsAlive}");
+            Console.WriteLine($"Приоритет потока: {thread.Priority}");
+            Console.WriteLine($"Статус потока: {thread.ThreadState}");
+            Console.WriteLine($"Статус потока: {thread.IsBackground}");
+            Console.WriteLine($"Статус потока: {thread.ThreadState}");
+            Console.WriteLine($"Статус потока: {thread.ThreadState}");
+            Console.WriteLine($"Домен приложения: {Thread.GetDomain().FriendlyName}");
+        }
+    }
+}
