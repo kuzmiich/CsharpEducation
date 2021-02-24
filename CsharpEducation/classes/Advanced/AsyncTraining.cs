3b821383641c86aa2c67350c937035c24778a4c1
иван кузьмич <ivankuzmic210@gmail.com>
2021-02-24 16:59:39 +0300

    Do async training task

--- CsharpEducation/classes/Advanced/AsyncTraining.cs
+++ CsharpEducation/classes/Advanced/AsyncTraining.cs
@@ -1,5 +1,6 @@
 ﻿using Education.interfaces;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -34,13 +35,71 @@ namespace Education.classes.Advanced.Multithreading
 
             await ReadFileAsync(path);
 
-            Console.WriteLine("1.Параллельный вызов асинхронных операций"); 
+            // 1
+            Console.WriteLine("-----1.Параллельный вызов асинхронных операций"); 
             AsyncTraining asyncTraining = new AsyncTraining();
-            Task task1 = asyncTraining.HelloAsync();
-            task1.Wait();
+            await asyncTraining.HelloAsync();
 
+            // 2
+            Console.WriteLine("-----2.Обработка ошибок в асинхронных методах\n" +
+                "Для обработки исключений используется конструкция try-catch-finally\n" +
+                "Для обработки нескольких исключений используется Task.WhenAll()\n" +
+                "В C# 6.0 добавили возможность await в блоке finally\n");
 
+            // 3
+            Console.WriteLine("-----3.Отмена асинхронных операций\n" +
+                "Для отмены асинхронных операций также используются классы CancellationToken и CancellationTokenSource.\n");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+            FactorialAsync(6, token);
+
+            cts.Cancel();
+            // 4
+            Console.WriteLine("-----4.Асинхронные стримы\n" +
+                "В версии C# 8.0 в C# были добавлены асинхронные стримы\n" +
+                "Для реализации используется yield и интерфейс IAsyncEnumberable<T> содержащийся в System.Collections.Generic\n");
+
+            await foreach (var number in GetNumbersAsync())
+            {
+                Console.WriteLine(number);
+            }
         }
+        public static async IAsyncEnumerable<int> GetNumbersAsync()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                // Thread.Sleep(200);
+                yield return i;
+            }    
+            
+        }
+
+        public static async Task FactorialAsync(int n, CancellationToken token)
+        {
+            if (token.IsCancellationRequested)
+            {
+                return;
+            }
+            await Task.Run(() => Factorial(n, token));
+        }
+
+        private static void Factorial(int n, CancellationToken token)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Операция прервана токеном");
+                    return;
+                }
+                result *= i;
+                Console.WriteLine($"Факториал числа {i} равен {result}");
+                Thread.Sleep(1000);
+            }
+        }
+
         public virtual async Task HelloAsync()
         {
             Task t1 = Task.Run(() => Hello());
@@ -49,10 +108,12 @@ namespace Education.classes.Advanced.Multithreading
             
             await Task.WhenAll(new[] { t1, t2, t3 });
         }
+
         private static void Hello()
         {
             Console.WriteLine("Hello world!");
         }
+
         public static void ReadFile(string path)
         {
             DateTime time = DateTime.Now;
